4a81743d0bd19d729a2b086ba37448b6d8bb02c9
Sami Pitkänen <Aquacrest@users.noreply.github.com>
2020-03-19 16:29:55 +0200

    Title screen with logo and buttons (Logo TBA)

--- app/src/main/java/fi/tuni/fullstack_quiz/TitleScreenActivity.java
+++ app/src/main/java/fi/tuni/fullstack_quiz/TitleScreenActivity.java
@@ -1,14 +1,22 @@
 package fi.tuni.fullstack_quiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class TitleScreenActivity extends AppCompatActivity {
+public class TitleScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void startGame(View v) {
+
+    }
+
+    public void submitQuestion(View v) {
+
     }
 }
